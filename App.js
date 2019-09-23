aebc72b1d3f2604edf3ea75e398b4699554976a7
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-23 15:25:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Class from './Class';
+import Effect from './Effect';
+import Custom from './Custom'
+import From from './From';
+//import Map from './Map';
+
+
+function CountApp() {
+    // Declare a new state variable, which we'll call "count"  
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <div><h1>Hooks</h1></div>
+            <div>
+                <p>You clicked {count} times</p> <br/>
+                <button onClick={() => setCount(count + 1)}>Click me</button>
+            </div>
+
+            <div>
+                <Class />
+                <Effect />
+                <Custom />
+                <From />
+             </div>
+        </div>
+        //<Map />
+    );
+}
+export default CountApp; 
---
+++ Class.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+class CountApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0 };
+    }
+    render() {
+        return (
+            <div>
+                <p><b>You clicked {this.state.count} times</b></p>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>Click me </button> <br />
+                <button onClick={() => this.setState({ count: this.state.count + 5 })}> Submit Me </button>
+            </div>
+        );
+    }
+}
+export default CountApp;
